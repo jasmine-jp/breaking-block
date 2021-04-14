2634b32fa3b7ece828cd46f1bb270a0fb62737dd
jasmine-jp <61972863+jasmine-jp@users.noreply.github.com>
2021-04-14 18:28:48 +0900

    game overを追加

--- main.py
+++ main.py
@@ -1,45 +1,53 @@
-basic.show_leds("""
-. . . # #
-. . . # #
-# . . # #
-. . . # #
-. . . # #
-""")
-
-y = 2
 def on_button_pressed_a():
     global y
-    led.plot(0, y-1)
+    led.plot(0, y - 1)
     if y > 0:
         led.unplot(0, y)
-        y -= 1
+        y += 0 - 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_button_pressed_b():
-    global y
-    led.plot(0, y+1)
-    if y < 4:
-        led.unplot(0, y)
-        y += 1
-input.on_button_pressed(Button.B, on_button_pressed_b)
+def shoot(n: number):
+    for i in range(1, n):
+        led.plot(i, y)
+        basic.pause(150)
+        led.unplot(i, y)
+        basic.pause(150)
 
 def on_button_pressed_ab():
     remove()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-def remove():
+def on_button_pressed_b():
     global y
+    led.plot(0, y + 1)
+    if y < 4:
+        led.unplot(0, y)
+        y += 1
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def remove():
     if led.point(3, y):
         shoot(3)
         led.unplot(3, y)
     else:
         shoot(4)
         led.unplot(4, y)
+y = 0
+basic.show_leds("""
+    . . . # #
+    . . . # #
+    # . . # #
+    . . . # #
+    . . . # #
+    """)
+y = 2
 
-def shoot(n):
-    global y
-    for i in range(1, n):
-        led.plot(i, y)
-        basic.pause(150)
-        led.unplot(i, y)
-        basic.pause(150)
+def on_forever():
+    flag = True
+    for j in range(5):
+        if led.point(j, 4):
+            flag = False
+    if flag:
+        basic.show_string("GAME OVER")
+        control.reset()
+forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
-basic.showLeds(`
-. . . # #
-. . . # #
-# . . # #
-. . . # #
-. . . # #
-`)
-let y = 2
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     led.plot(0, y - 1)
     if (y > 0) {
         led.unplot(0, y)
-        y -= 1
+        y += 0 - 1
     }
     
+})
+function shoot(n: number) {
+    for (let i = 1; i < n; i++) {
+        led.plot(i, y)
+        basic.pause(150)
+        led.unplot(i, y)
+        basic.pause(150)
+    }
+}
+
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    remove()
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
@@ -24,11 +28,7 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     }
     
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    remove()
-})
 function remove() {
-    
     if (led.point(3, y)) {
         shoot(3)
         led.unplot(3, y)
@@ -39,13 +39,26 @@ function remove() {
     
 }
 
-function shoot(n: number) {
-    
-    for (let i = 1; i < n; i++) {
-        led.plot(i, y)
-        basic.pause(150)
-        led.unplot(i, y)
-        basic.pause(150)
+let y = 0
+basic.showLeds(`
+    . . . # #
+    . . . # #
+    # . . # #
+    . . . # #
+    . . . # #
+    `)
+y = 2
+forever(function on_forever() {
+    let flag = true
+    for (let j = 0; j < 5; j++) {
+        if (led.point(j, 4)) {
+            flag = false
+        }
+        
     }
-}
-
+    if (flag) {
+        basic.showString("GAME OVER")
+        control.reset()
+    }
+    
+})
