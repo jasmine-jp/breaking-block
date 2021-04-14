d80a9e4bb910583a0aac37a2780b2d757001e450
jasmine-jp <61972863+jasmine-jp@users.noreply.github.com>
2021-04-14 18:15:37 +0900

    shootを追加

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def on_button_pressed_a():
     if y > 0:
         led.unplot(0, y)
         y -= 1
-    remove(y)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
@@ -22,11 +21,25 @@ def on_button_pressed_b():
     if y < 4:
         led.unplot(0, y)
         y += 1
-    remove(y)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def remove(int: y):
+def on_button_pressed_ab():
+    remove()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def remove():
+    global y
     if led.point(3, y):
+        shoot(3)
         led.unplot(3, y)
     else:
+        shoot(4)
         led.unplot(4, y)
+
+def shoot(n):
+    global y
+    for i in range(1, n):
+        led.plot(i, y)
+        basic.pause(150)
+        led.unplot(i, y)
+        basic.pause(150)
--- main.ts
+++ main.ts
@@ -1,2 +1,51 @@
-number n = 10;
-basic.showNumber(n)
+basic.showLeds(`
+. . . # #
+. . . # #
+# . . # #
+. . . # #
+. . . # #
+`)
+let y = 2
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    led.plot(0, y - 1)
+    if (y > 0) {
+        led.unplot(0, y)
+        y -= 1
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    led.plot(0, y + 1)
+    if (y < 4) {
+        led.unplot(0, y)
+        y += 1
+    }
+    
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    remove()
+})
+function remove() {
+    
+    if (led.point(3, y)) {
+        shoot(3)
+        led.unplot(3, y)
+    } else {
+        shoot(4)
+        led.unplot(4, y)
+    }
+    
+}
+
+function shoot(n: number) {
+    
+    for (let i = 1; i < n; i++) {
+        led.plot(i, y)
+        basic.pause(150)
+        led.unplot(i, y)
+        basic.pause(150)
+    }
+}
+
